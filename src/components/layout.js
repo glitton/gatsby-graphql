43d513458c73d468823c1ff5e084fb1365f3d00d
Generosa Litton <glcodeworks@gmail.com>
2022-03-06 15:03:29 -0800

    no page title in layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,11 +2,10 @@ import * as React from "react";
 import Navigation from "./navigation";
 import { container, heading } from "./layout.module.css";
 
-const Layout = ({ pageTitle, children }) => {
+const Layout = ({ children }) => {
   return (
     <div className={container}>
       <Navigation />
-      <title>{pageTitle}</title>
       <main>
         <h1 className={heading}>{pageTitle}</h1>
         {children}
