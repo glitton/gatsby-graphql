8ea463677fbe1cd6fb61c3152dd8d01439472f01
Generosa Litton <glcodeworks@gmail.com>
2022-03-06 15:00:06 -0800

    removed images, add later

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,15 +1,10 @@
 import React from "react";
 import { Link } from "gatsby";
-import { StaticImage } from "gatsby-plugin-image";
 import { navLinks, navLinkItem, navLinkText } from "./layout.module.css";
 
 const Navigation = () => {
   return (
     <nav>
-      <div>
-        <StaticImage alt="Gatsby logo" src="../images/gatsby.png" />
-        <StaticImage alt="GraphQL logo" src="../images/graphql.png" />
-      </div>
       <ul className={navLinks}>
         <li className={navLinkItem}>
           <Link to="/" className={navLinkText}>
