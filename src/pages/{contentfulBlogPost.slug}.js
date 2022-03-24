f180742de092bd627d687f504c7c4f010c9706f9
Generosa Litton <glcodeworks@gmail.com>
2022-03-24 15:20:16 -0500

    blog template wip

---
+++ src/pages/{contentfulBlogPost.slug}.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../components/layout";
+
+const BlogPostTemplate = () => {
+  return (
+    <Layout pageTitle="Placeholder">
+      <p>Individual posts go here</p>
+    </Layout>
+  );
+};
+
+export default BlogPostTemplate;
